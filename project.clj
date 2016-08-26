4c63b620d50515f9fb5ae3c5b4b96ae87df07c34
Daniel Cunha <danielsoro@gmail.com>
2016-08-25 23:02:17 -0300

    Removed plugin. Updated license

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
 ;you may not use this file except in compliance with the License.
 ;You may obtain a copy of the License at
 ;
-;http://www.apache.org/licenses/LICENSE-2.0
+;    http://www.apache.org/licenses/LICENSE-2.0
 ;
 ;Unless required by applicable law or agreed to in writing, software
 ;distributed under the License is distributed on an "AS IS" BASIS,
@@ -19,5 +19,4 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [com.draines/postal "2.0.0"]
                  [environ "1.1.0"]]
-  :plugins [[lein-environ "1.1.0"]]
   :main mail-send.core)
